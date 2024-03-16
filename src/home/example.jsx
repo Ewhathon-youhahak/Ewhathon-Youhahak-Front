9954da8637fd807ba606855418c4c7e44a48c271
Seoyoung Oh <ohseoyoung5@gmail.com>
2024-03-16 12:33:14 +0900

    홈화면 기능 구현

--- src/home/example.jsx
+++
@@ -1,9 +0,0 @@
-function Example() {
-  return (
-    <div>
-      <h1>Hello, React!</h1>
-    </div>
-  );
-}
-
-export default Example;
--- src/home/index.js
+++ src/home/index.js
@@ -1,6 +1,26 @@
 import React from 'react';
-import Example from './example';
+// import Example from './example';
+import RadioButtonAndSearch from './radioButtonAndSearch';
+import DocumentList from './documentList';
 
 export const Home = () => {
-    return <Example />;
+    return (
+        <div style={containerStyle}>
+            <RadioButtonAndSearch />
+            <div style={documentListContainerStyle}>
+                <DocumentList />
+            </div>
+        </div>
+    );
 };
+
+const containerStyle = {
+    display: 'flex', // 가로 정렬을 위해 flex 사용
+    flexDirection: 'column', // 세로 방향으로 요소 배치
+    alignItems: 'center', // 모든 요소를 가로 기준 가운데 정렬
+    margin: '50px 100px 0 100px',
+    };
+
+const documentListContainerStyle = {
+    alignSelf: 'flex-start', // DocumentList를 왼쪽으로 붙임
+  };
