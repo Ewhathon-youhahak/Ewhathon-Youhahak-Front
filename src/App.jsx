94fadec9ab0df71d1999051eb95ec81f0e83638a
Seoyoung Oh <ohseoyoung5@gmail.com>
2024-03-16 15:53:29 +0900

    마이페이지 기본 구조

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import * as React from "react";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Mypage } from './mypage'; 
 
 function App() {
   return (
     <Router>
       <div className="APP">
         <Routes>
-
+        <Route path="/myPage" element={<Mypage />} />
         </Routes>
       </div>
     </Router>
